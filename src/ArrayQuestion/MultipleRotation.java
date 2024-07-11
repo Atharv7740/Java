db0a928c1bd5fae98cea8fb0d8b84d51571d2a79
Atharv7740 <atharv7740@gmail.com>
2024-07-11 13:25:00 +0530

    update

--- src/ArrayQuestion/MultipleRotation.java
+++ src/ArrayQuestion/MultipleRotation.java
@@ -1,4 +1,33 @@
 package ArrayQuestion;
 
 public class MultipleRotation {
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        int b[]={2,3};
+
+        int ans[][]=new int[b.length][a.length];
+
+
+    }
+    static void printArray(int a[]){
+        for(int e: a ){
+            System.out.println(e);
+        }
+
+    }
+
+
+    static void rotateFunction(int a[], int k){
+        for(int i=0;i<k;i++){
+            int temp=a[i];
+            int n=a.length;
+            for (int j=0; j<n-1; j++){
+                a[j]=a[j+1];
+            }
+            a[n-1]=temp;
+        }
+
+
+    }
+
 }
---
+++ src/package_1/TrailClass.java
@@ -0,0 +1,14 @@
+package package_1;
+
+public class TrailClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+
+}
+
+
+
+
+
