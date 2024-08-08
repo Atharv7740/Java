422b8960ae0cc719d99a2c7591299a1796d2f354
Atharv7740 <atharv7740@gmail.com>
2024-08-08 18:07:10 +0530

    update

---
+++ src/AlgoPrepArray/PairExist.java
@@ -0,0 +1,34 @@
+package AlgoPrepArray;
+
+import java.util.Scanner;
+
+public class PairExist {
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            int k=sc.nextInt();
+            System.out.println(pairExist(arr,k));
+
+        }
+        public static boolean pairExist(int[] arr, int k){
+            for(int i=0;i<arr.length;i++){
+                int required=k-arr[i];
+                for(int j=i+1;j<arr.length;j++){
+                    if(arr[j]==required){
+                        return true;
+                    }
+
+                }
+
+            }
+            return false;
+
+        }
+        //No more dsa today
+
+}
