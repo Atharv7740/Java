2c46fdc0e66d0a05786e1eb6de52e95b498c8575
Atharv7740 <atharv7740@gmail.com>
2024-08-13 20:43:56 +0530

    update

--- src/AlgoPrepArray/PairExist.java
+++ src/AlgoPrepArray/PairExist.java
@@ -6,13 +6,13 @@ public class PairExist {
 
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
-            System.out.println("Enter the value of N: ");
+            System.out.println("Enter the value of n: ");
             int n=sc.nextInt();
             int[] arr=new int[n];
             for(int i=0;i<n;i++){
                 arr[i]=sc.nextInt();
             }
-            System.out.println("Enter the value of K: ");
+            System.out.println("Enter the value of k: ");
             int k=sc.nextInt();
             System.out.println(pairExist(arr,k));
 
