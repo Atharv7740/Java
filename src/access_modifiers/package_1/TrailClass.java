cd2bdf181e6bc355eb7972f44d500f70f20a0798
Atharv7740 <atharv7740@gmail.com>
2024-08-17 19:35:11 +0530

    update

--- src/access_modifiers/package_1/TrailClass.java
+++ src/access_modifiers/package_1/TrailClass.java
@@ -5,9 +5,11 @@ public class TrailClass extends MultipleRotation {
     protected int var1;
     public int var2;
 
+
     public static void main(String[] args) {
         System.out.println("hello world");
         TrailClass2 obj = new TrailClass2();
+        System.out.println("No error");
 
 
 
